8d6b59921f7ee4f392bf34fc797c8a710ba88f2d
FabioKido <fabiohenryquemesquita@gmail.com>
2023-03-15 10:20:09 -0300

    Updated: add emojis tool

--- src/Component.svelte
+++ src/Component.svelte
@@ -2,7 +2,7 @@
   import { getContext, onDestroy } from "svelte";
 
   import CKEditor from "ckeditor5-svelte";
-  import CustomEditor from "ckeditor5-36.0.1-gukvvkxh7pge/build/ckeditor";
+  import CustomEditor from "ckeditor5-36.0.1-gukvvkxh5fgh/build/ckeditor";
 
   import "./custom.css";
 
